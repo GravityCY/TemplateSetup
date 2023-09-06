ec90246f5bf2bc7e9e5de9d67f5d91042d842489
harry <harryioannoucy@gmail.com>
2023-09-06 14:03:15 +0300

    init

---
+++ default/copy/src/main/java/me/gravityio/@{mod_package_name}/@{mod_class_name}.java
@@ -0,0 +1,15 @@
+package me.gravityio.@{mod_package_name};
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class @{mod_class_name} implements ModInitializer {
+    public static String MOD_ID = "@{mod_id}";
+    public static Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
